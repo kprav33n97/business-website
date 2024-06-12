9b8eb6fc2f13c5e138d0f20042d7bbd0af3b8832
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-06-12 17:41:47 +0530

    Programs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <div className="container">
+       <Title subTitle="Our Programs" title="What we offer" />  
+       <Programs/>
+      </div>
     </>
   )
 }

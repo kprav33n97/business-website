6360d921b6da495c7e7feaebfa46b56823881224
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-07-19 09:26:03 +0530

    Update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero container'>
+    <div className='hero'>
       <div className="hero-text">
         <h1>We ensure best solutions for every business!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero minus maxime corporis dicta earum et iste ipsam harum, veniam hic dolores nihil.</p>
